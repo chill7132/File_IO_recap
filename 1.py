e17ff27102de861569e7b71923e9c0f1cd7390e3
Callum <chill7132@sohamcollege.org.uk>
2014-10-14 09:38:52 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,7 @@
 #Write a shopping list in Notepad and save it as 'shopping.txt'. Write a program to read in the shopping list from the file and display it on the screen.
+import time
+def ShoppingList(): 
+    with open("Shoppinglist.txt",mode="r",encoding="utf-8")as my_file:
+        for line in my_file:
+                time.sleep(3) 
+                print(line)
